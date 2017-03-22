15868420d8aa2b08663cbb5d225e3ca15a9be0dc
victorHuguw <kibedasarabia@gmail.com>
2017-03-22 16:02:27 -0400

    dd

---
+++ lista02_exercicio04.py
@@ -0,0 +1,8 @@
+#4)Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+#
+n1 = int(input("digite sua primeira nota bimestral:"))
+n2 = int(input("digite sua segunda nota bimestral:"))
+n3 = int(input("digite sua terceira nota bimestral:"))
+n4 = int(input("digite sua quarta nota bimestral:"))
+result_media = (n1+n2+n3+n4)/4
+print("a sua media e :", result_media)
