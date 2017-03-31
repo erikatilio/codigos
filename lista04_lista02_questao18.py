b5039bd33b974372ab281796709ddec02a1f26b1
VictorHuguw <kibedasarabia@gmail.com>
2017-03-31 10:36:14 -0300

    Create lista04_lista02_questao18.py

---
+++ lista04_lista02_questao18.py
@@ -0,0 +1,12 @@
+#URI-Online Judge
+#
+#1008 - Salário
+
+Number_of_an_employee = float(input())
+worked_hours = float(input())
+value_per_hour = float(input())
+
+salary = worked_hours * value_per_hour
+
+print("NUMBER = %i" % Number_of_an_employee) 
+print("SALARY = U$ %5.2f" % salary)
